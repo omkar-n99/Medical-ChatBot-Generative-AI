1c27541d31f52a77256a44c1485e37c13c37be2b
st196591 <st196591@stud.uni-stuttgart.de>
2025-10-08 23:14:04 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Omkar Narvade",
+    author_email="nomkar99@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
